dd5efc971849785d3ab423b7cd00cee8b76e2417
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2019-02-25 15:43:17 +0100

    s3 test accroding to example

--- GIRAFFE/code/workflow.py
+++ GIRAFFE/code/workflow.py
@@ -11,13 +11,17 @@ import nipype.algorithms.confounds as confounds
 import nipype.interfaces.utility as utility
 
 #Generic datagrabber module that wraps around glob in an
-io_S3DataGrabber = pe.Node(io.S3DataGrabber(outfields=["outfiles"]), name = 'io_S3DataGrabber')
+io_S3DataGrabber = pe.Node(io.S3DataGrabber(infields=["subj_id, run_num, field_template"], outfields=["func, struct"]), name = 'io_S3DataGrabber')
 io_S3DataGrabber.inputs.bucket = 'openneuro'
 io_S3DataGrabber.inputs.sort_filelist = True
-io_S3DataGrabber.inputs.template = 'sub-01/func/sub-01_task-simon_run-1_bold.nii.gz'
+io_S3DataGrabber.inputs.template = '*'
 io_S3DataGrabber.inputs.anon = True
 io_S3DataGrabber.inputs.bucket_path = 'ds000101/ds000101_R2.0.0/uncompressed/'
 io_S3DataGrabber.inputs.local_directory = '/tmp'
+io_S3DataGrabber.inputs.subj_id = ['sub001', 'sub002']
+io_S3DataGrabber.inputs.run_num = ['run001', 'run003']
+io_S3DataGrabber.inputs.field_template = dict(func='%s/BOLD/task001_%s/bold.nii.gz',                                     struct='%s/anatomy/highres001_brain.nii.gz')
+io_S3DataGrabber.inputs.template_args =  dict(         func=[['subj_id', 'run_num']], struct=[['subj_id']])
 
 #Wraps command **slicetimer**
 fsl_SliceTimer = pe.Node(interface = fsl.SliceTimer(), name='fsl_SliceTimer', iterfield = [''])
@@ -53,9 +57,15 @@ fsl_TemporalFilter.inputs.highpass_sigma = 25
 utility_Rename = pe.Node(interface = utility.Rename(), name='utility_Rename', iterfield = [''])
 utility_Rename.inputs.format_string = "/output/filtered.nii.gz"
 
+#Wraps the executable command ``bet``.
+fsl_BET = pe.Node(interface = fsl.BET(), name='fsl_BET', iterfield = [''])
+
+#Change the name of a file based on a mapped format string.
+utility_Rename_1 = pe.Node(interface = utility.Rename(), name='utility_Rename_1', iterfield = [''])
+utility_Rename_1.inputs.format_string = "/output/skullstripped.nii.gz"
+
 #Create a workflow to connect all those nodes
 analysisflow = nipype.Workflow('MyWorkflow')
-analysisflow.connect(io_S3DataGrabber, "outfiles", fsl_SliceTimer, "in_file")
 analysisflow.connect(fsl_SliceTimer, "slice_time_corrected_file", fsl_MCFLIRT, "in_file")
 analysisflow.connect(fsl_MCFLIRT, "out_file", confounds_TSNR, "in_file")
 analysisflow.connect(confounds_TSNR, "stddev_file", fsl_ImageStats, "in_file")
@@ -67,6 +77,9 @@ analysisflow.connect(confounds_TSNR, "detrended_file", fsl_FilterRegressor, "in_
 analysisflow.connect(fsl_FilterRegressor, "out_file", fsl_TemporalFilter, "in_file")
 analysisflow.connect(confounds_TSNR, "stddev_file", fsl_Threshold, "in_file")
 analysisflow.connect(fsl_TemporalFilter, "out_file", utility_Rename, "in_file")
+analysisflow.connect(io_S3DataGrabber, "func", fsl_SliceTimer, "in_file")
+analysisflow.connect(io_S3DataGrabber, "struct", fsl_BET, "in_file")
+analysisflow.connect(fsl_BET, "out_file", utility_Rename_1, "in_file")
 
 #Run the workflow
 plugin = 'MultiProc' #adjust your desired plugin here
