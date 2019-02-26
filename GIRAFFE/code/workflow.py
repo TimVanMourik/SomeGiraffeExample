68af074b25a9255321c1e28583b3c3fe762ca865
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2019-02-26 12:36:48 +0100

    Parameter addition and rename

--- GIRAFFE/code/workflow.py
+++ GIRAFFE/code/workflow.py
@@ -11,62 +11,74 @@ import nipype.algorithms.confounds as confounds
 import nipype.interfaces.utility as utility
 
 #Generic datagrabber module that wraps around glob in an
-io_S3DataGrabber = pe.Node(io.S3DataGrabber(outfields=["outfiles"]), name = 'io_S3DataGrabber')
-io_S3DataGrabber.inputs.bucket = 'openneuro'
-io_S3DataGrabber.inputs.sort_filelist = True
-io_S3DataGrabber.inputs.template = 'sub-01/func/sub-01_task-simon_run-1_bold.nii.gz'
-io_S3DataGrabber.inputs.anon = True
-io_S3DataGrabber.inputs.bucket_path = 'ds000101/ds000101_R2.0.0/uncompressed/'
-io_S3DataGrabber.inputs.local_directory = '/tmp'
+DataFromOpenNeuro = pe.Node(io.S3DataGrabber(infields=["subj_id", "run_num", "field_template"], outfields=["func", "struct"]), name = 'DataFromOpenNeuro')
+DataFromOpenNeuro.inputs.bucket = 'openfmri'
+DataFromOpenNeuro.inputs.sort_filelist = True
+DataFromOpenNeuro.inputs.template = '*'
+DataFromOpenNeuro.inputs.anon = True
+DataFromOpenNeuro.inputs.bucket_path = 'ds001/'
+DataFromOpenNeuro.inputs.local_directory = '/tmp'
+DataFromOpenNeuro.inputs.field_template = dict(func='%s/BOLD/task001_%s/bold.nii.gz', struct='%s/anatomy/highres001_brain.nii.gz')
+DataFromOpenNeuro.inputs.template_args =  dict(func=[['subj_id', 'run_num']], struct=[['subj_id']])
 
 #Wraps command **slicetimer**
-fsl_SliceTimer = pe.Node(interface = fsl.SliceTimer(), name='fsl_SliceTimer', iterfield = [''])
+SliceTimer = pe.MapNode(interface = fsl.SliceTimer(), name='SliceTimer', iterfield = ['in_file'])
 
 #Wraps command **mcflirt**
-fsl_MCFLIRT = pe.Node(interface = fsl.MCFLIRT(), name='fsl_MCFLIRT', iterfield = [''])
+MotionCorrection = pe.MapNode(interface = fsl.MCFLIRT(), name='MotionCorrection', iterfield = ['in_file'])
 
 #Computes the time-course SNR for a time series
-confounds_TSNR = pe.Node(interface = confounds.TSNR(), name='confounds_TSNR', iterfield = [''])
-confounds_TSNR.inputs.regress_poly = 3
+TSNR = pe.MapNode(interface = confounds.TSNR(), name='TSNR', iterfield = ['in_file'])
+TSNR.inputs.regress_poly = 3
 
 #Wraps command **fslstats**
-fsl_ImageStats = pe.Node(interface = fsl.ImageStats(), name='fsl_ImageStats', iterfield = [''])
-fsl_ImageStats.inputs.op_string = '-p 98'
+ComputeLowTsnr = pe.MapNode(interface = fsl.ImageStats(), name='ComputeLowTsnr', iterfield = ['in_file'])
+ComputeLowTsnr.inputs.op_string = '-p 98'
 
 #Wraps command **fslmaths**
-fsl_Threshold = pe.Node(interface = fsl.Threshold(), name='fsl_Threshold', iterfield = [''])
-fsl_Threshold.inputs.args = '-bin'
+Threshold = pe.MapNode(interface = fsl.Threshold(), name='Threshold', iterfield = ['thresh', 'in_file'])
+Threshold.inputs.args = '-bin'
 
 #Anatomical compcor: for inputs and outputs, see CompCor.
-confounds_ACompCor = pe.Node(interface = confounds.ACompCor(), name='confounds_ACompCor', iterfield = [''])
-confounds_ACompCor.inputs.num_components = 2
+NoiseComponents = pe.MapNode(interface = confounds.ACompCor(), name='NoiseComponents', iterfield = ['realigned_file', 'mask_files'])
+NoiseComponents.inputs.num_components = 2
 
 #Wraps command **fsl_regfilt**
-fsl_FilterRegressor = pe.Node(interface = fsl.FilterRegressor(), name='fsl_FilterRegressor', iterfield = [''])
-fsl_FilterRegressor.inputs.filter_columns = [1, 2]
+RegressionFilter = pe.MapNode(interface = fsl.FilterRegressor(), name='RegressionFilter', iterfield = ['in_file', 'design_file'])
+RegressionFilter.inputs.filter_columns = [1, 2]
 
 #Wraps command **fslmaths**
-fsl_TemporalFilter = pe.Node(interface = fsl.TemporalFilter(), name='fsl_TemporalFilter', iterfield = [''])
-fsl_TemporalFilter.inputs.highpass_sigma = 25
+BandpassFilter = pe.MapNode(interface = fsl.TemporalFilter(), name='BandpassFilter', iterfield = ['in_file'])
+BandpassFilter.inputs.highpass_sigma = 25
 
-#Change the name of a file based on a mapped format string.
-utility_Rename = pe.Node(interface = utility.Rename(), name='utility_Rename', iterfield = [''])
-utility_Rename.inputs.format_string = "/output/filtered.nii.gz"
+#Wraps the executable command ``bet``.
+BrainExtraction = pe.MapNode(interface = fsl.BET(), name='BrainExtraction', iterfield = ['in_file'])
+
+#Generic datasink module to store structured outputs
+io_DataSink = pe.Node(interface = io.DataSink(), name='io_DataSink')
+io_DataSink.inputs.base_directory = '/output/'
+
+#Basic interface class generates identity mappings
+(utility.IdentityInterface(fields=['']), name='Parameters', iterfield = [''])
 
 #Create a workflow to connect all those nodes
 analysisflow = nipype.Workflow('MyWorkflow')
-analysisflow.connect(io_S3DataGrabber, "outfiles", fsl_SliceTimer, "in_file")
-analysisflow.connect(fsl_SliceTimer, "slice_time_corrected_file", fsl_MCFLIRT, "in_file")
-analysisflow.connect(fsl_MCFLIRT, "out_file", confounds_TSNR, "in_file")
-analysisflow.connect(confounds_TSNR, "stddev_file", fsl_ImageStats, "in_file")
-analysisflow.connect(fsl_ImageStats, "out_stat", fsl_Threshold, "thresh")
-analysisflow.connect(fsl_MCFLIRT, "out_file", confounds_ACompCor, "realigned_file")
-analysisflow.connect(fsl_Threshold, "out_file", confounds_ACompCor, "mask_files")
-analysisflow.connect(confounds_ACompCor, "components_file", fsl_FilterRegressor, "design_file")
-analysisflow.connect(confounds_TSNR, "detrended_file", fsl_FilterRegressor, "in_file")
-analysisflow.connect(fsl_FilterRegressor, "out_file", fsl_TemporalFilter, "in_file")
-analysisflow.connect(confounds_TSNR, "stddev_file", fsl_Threshold, "in_file")
-analysisflow.connect(fsl_TemporalFilter, "out_file", utility_Rename, "in_file")
+analysisflow.connect(SliceTimer, "slice_time_corrected_file", MotionCorrection, "in_file")
+analysisflow.connect(MotionCorrection, "out_file", TSNR, "in_file")
+analysisflow.connect(TSNR, "stddev_file", ComputeLowTsnr, "in_file")
+analysisflow.connect(ComputeLowTsnr, "out_stat", Threshold, "thresh")
+analysisflow.connect(MotionCorrection, "out_file", NoiseComponents, "realigned_file")
+analysisflow.connect(Threshold, "out_file", NoiseComponents, "mask_files")
+analysisflow.connect(NoiseComponents, "components_file", RegressionFilter, "design_file")
+analysisflow.connect(TSNR, "detrended_file", RegressionFilter, "in_file")
+analysisflow.connect(RegressionFilter, "out_file", BandpassFilter, "in_file")
+analysisflow.connect(TSNR, "stddev_file", Threshold, "in_file")
+analysisflow.connect(DataFromOpenNeuro, "func", SliceTimer, "in_file")
+analysisflow.connect(DataFromOpenNeuro, "struct", BrainExtraction, "in_file")
+analysisflow.connect(BandpassFilter, "out_file", io_DataSink, "filtered")
+analysisflow.connect(BrainExtraction, "out_file", io_DataSink, "skullstripped")
+analysisflow.connect(Parameters, "subj_id", DataFromOpenNeuro, "subj_id")
+analysisflow.connect(Parameters, "run_num", DataFromOpenNeuro, "run_num")
 
 #Run the workflow
 plugin = 'MultiProc' #adjust your desired plugin here
